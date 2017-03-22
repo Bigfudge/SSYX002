25c1c928e41036e8abe256c5874b339d9371d5d2
Mathias1988 <mathias1988@yahoo.se>
2017-03-22 15:27:41 +0100

    Mindre uppdateringar

--- Arduino_UNO_draft/Arduino_UNO_draft.ino
+++ Arduino_UNO_draft/Arduino_UNO_draft.ino
@@ -38,6 +38,10 @@ ros::Publisher pub_spdV("speedV", &spdV_msg);
 int fram = 16736925;     // Pilknapp framåt
 int ok = 16712445;       // Mittenknapp
 int forts = 4294967295;  // Vid nedtryckt knapp
+volatile byte countH = 0; // Räknar antal pulser på höger sida
+volatile byte countV = 0; // Räknar antal pulser på vänster sida
+int speedH = 0;          // Hastighet höger sida
+int speedV = 0;          // Hastighet vänster sida
 
 IRrecv irrecv(11);    // Tar emot IR-signaler på pin 11
 decode_results results;
@@ -56,42 +60,58 @@ void setup()
 
   motor_H.writeMicroseconds(1500);    // Ser till att båda motorer står still innan de får signal
   motor_V.writeMicroseconds(1500);
+
+  pinMode(2, INPUT);  // Insignal för höger impulsgivare
+  pinMode(3, INPUT);  // Insignal för vänster impulsgivare
+  attachInterrupt(digitalPinToInterrupt(2), right, RISING); // Skapar interrupt för stigande flank på pulsen
+  attachInterrupt(digitalPinToInterrupt(3), left, RISING);
 }
 long publisher_timer;
 
 void loop()
 {
+  if (millis() > publisher_timer) {
+    // steg 1: kontrollera antal pulser sen senast och konvertera till hastighet
+    speedH = (countH * 1000 / (1000 + millis() - publisher_timer));   // Antal pulser per sekund
+    speedV = (countV * 1000 / (1000 + millis() - publisher_timer));
+    speedH = speedH;    // Justering för antal pulser per meter
+    speedV = speedV;
+    //------------------------------------------------Kod måste läggas till för att kunna läsa encodrarna
+    spdH_msg.data = speedH;
+    spdV_msg.data = speedV;
+    pub_spdH.publish(&spdH_msg);
+    pub_spdV.publish(&spdV_msg);
+    
+    countH = 0;   // Nollställer räknare
+    countV = 0;
+  }
+
+  publisher_timer = millis() + 1000; // Publisera en gång per sekund
+  nh.spinOnce();
+
   if (irrecv.decode(&results))  // När det kommer en IR-signal
   {
-    if (millis() > publisher_timer) {
-      // step 1: request reading from sensor
-      int speedH=0;
-      int speedV=0;
-       //------------------------------------------------Kod måste läggas till för att kunna läsa encodrarna
-      spdH_msg.data = speedH;
-      spdV_msg.data = speedV;
-      pub_spdH.publish(&spdH_msg);
-      pub_spdV.publish(&spdV_msg);
-    }
-
-    publisher_timer = millis() + 1000; //publish once a second
-  }
-  nh.spinOnce();
-  delay(1);
-
-  if (results.value == ok)    // Nödstopp när man trycker på mittenknappen, nollställs genom att trycka pil upp.
-  {
-    while (results.value != fram)
+    if (results.value == ok)    // Nödstopp när man trycker på mittenknappen, nollställs genom att trycka pil upp.
     {
-      motor_H.writeMicroseconds(1500);    // Stannar båda motorerna
-      motor_V.writeMicroseconds(1500);
+      while (results.value != fram)
+      {
+        motor_H.writeMicroseconds(1500);    // Stannar båda motorerna
+        motor_V.writeMicroseconds(1500);
 
-      irrecv.resume();                    // Förbereder för att ta emot nästa värde
-      delay(100);                         // Paus 100ms för att hinna starta om IR
-      irrecv.decode(&results);            // Tar emot nästa värde
+        irrecv.resume();                    // Förbereder för att ta emot nästa värde
+        delay(100);                         // Paus 100ms för att hinna starta om IR
+        irrecv.decode(&results);            // Tar emot nästa värde
 
-      Serial.println("Nödstopp aktiv");   // Skriver ut att nödstoppen är aktiv i serial monitor
+        Serial.println("Nödstopp aktiv");   // Skriver ut att nödstoppen är aktiv i serial monitor
+      }
+      irrecv.resume();                      // Tar emot nästa värde
     }
-    irrecv.resume();                      // Tar emot nästa värde
   }
+}
+
+void right() {        // Ökar räknaren för höger sida med ett för varje puls
+  countH = ++countH;
+}
+void left() {         // Ökar räknaren för vänster sida med ett för varje puls
+  countV = ++countV;
 }
